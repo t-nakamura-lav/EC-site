37294014441451097415a1c6fcc0f4d8d06812ff
t-nakamura <1am1goodtime@gmail.com>
2021-10-12 09:07:02 +0000

    customer devise設定

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  #customer側devise
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+  
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
