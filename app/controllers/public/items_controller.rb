765cd17d7c7831d66c2c86c4d5b5106d4895f47a
t-nakamura <1am1goodtime@gmail.com>
2021-10-13 04:39:46 +0000

    customer item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,25 @@
 class Public::ItemsController < ApplicationController
   def top
+    @items = Item.all
+  end
+  
+  def about
   end
 
   def index
-  end
-  
-  def show
+    @items = Item.all
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+
+
+
   private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :genre_id)
+  end
 end
